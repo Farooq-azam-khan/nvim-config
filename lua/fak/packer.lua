7b4c681558547e938dc537fc469901fb0f105a7f
farooq-azam-khan <khanazamfarooq99@gmail.com>
2023-02-20 16:11:48 -0500

    added treesitter

--- lua/fak/packer.lua
+++ lua/fak/packer.lua
@@ -4,16 +4,17 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  -- use 'neovim/nvim-lspconfig' -- common lsp config
-  use 'folke/tokyonight.nvim'
-  use { 'neoclide/coc.nvim', branch='release' }
-  use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.x',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    -- use 'neovim/nvim-lspconfig' -- common lsp config
+    use 'folke/tokyonight.nvim'
+    use { 'neoclide/coc.nvim', branch='release' }
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.x',
+    -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use('nvim-treesitter/nvim-treesitter', {run=':TSUpdate'})
 end)
  
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "/home/farooq-azam-khan/.local/share/nvim/site/pack/packer/start/coc.nvim",
     url = "https://github.com/neoclide/coc.nvim"
   },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/home/farooq-azam-khan/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/farooq-azam-khan/.local/share/nvim/site/pack/packer/start/packer.nvim",
