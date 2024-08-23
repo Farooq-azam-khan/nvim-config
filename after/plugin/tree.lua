d881c0b86f570d3cd461daefbe1eff750655f01b
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-08-22 23:02:02 -0400

    tree.lua

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -5,10 +5,23 @@ vim.g.loaded_netrwPlugin = 1
 -- 24-bit colour
 vim.opt.termguicolors = true
 
-require('nvim-tree').setup({
-    sort={sorted='case_sensitive',},
-    view={width=30,}
-    renderer={group_empty=true},
-    filter={dotfiles=true},
+local tree = require('nvim-tree')
+local api = require('nvim-tree.api')
+local function on_attach(bufnr)
+    local function opts(desc)
+        return {desc = 'nvim-tree: ' .. desc, buffer=bufnr, noremap=true,silent=true,nowait=true}
+    end
+    api.config.mappings.default_on_attach(bufnr)
+    vim.keymap.set('n', '<C-t>',api.tree.change_root_to_parent, opts('Up'))
+    vim.keymap.set('n', '?', api.tree.toggle_help,opts('Help'))
 
+end
+tree.setup({
+    --sort={sorter='case_sensitive',},
+   -- view={width=30,},
+    --renderer={group_empty=true},
+    --filters={dotfiles=true},
+    on_attach=on_attach,
 })
+
+vim.keymap.set('n', '<C-b>', function () api.tree.toggle() end)
