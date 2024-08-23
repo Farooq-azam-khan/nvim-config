1f629c134916c9d4fc6979e175ed05c9414cc977
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-08-22 22:16:41 -0400

    lsp-zero-v4

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,27 +1,78 @@
-local lsp = require('lsp-zero')
-require('mason').setup()
-require('mason-lspconfig').setup({
-    ensure_installed = {'eslint', 'tsserver', 'rust_analyzer', 'ruff', 'elmls', 'tailwindcss'}
+local lsp_zero = require('lsp-zero')
+local lsp_attach = function(client, bufnr) 
+    lsp_zero.default_keymaps({buffer=bufnr})
+end 
+
+lsp_zero.extend_lspconfig({
+    capabilities=require('cmp_nvim_lsp').default_capabilities(),
+    lsp_attach = lsp_attach, 
+    ifloat_border='rounded',
+    sign_text = {
+        error = '✘',
+        warn = '▲',
+        hint = '⚑',
+        info = ''
+    },
 })
-----lsp.preset('recommended')
---
---lsp.ensure_installed({
---     "eslint", "tsserver",
---     "rust_analyzer", 
---     "pylsp",  
---
---})
---
---local cmp = require('cmp')
---local cmp_select = {behavior = cmp.SelectBehavior.Select}
---local cmp_mappings = lsp.defaults.cmp_mappings({
---    -- Only exists for lifetime of buffer 
---    -- i.e. any mappings used below will not override other mappings
---    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select), 
---    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select), 
---    ["<C-y>"] = cmp.mapping.confirm({select=true}), 
---    ["<C-Space>"] = cmp.mapping.complete(),
---})
+
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = {'eslint', 'tsserver', 'rust_analyzer', 'ruff', 'elmls', 'tailwindcss'},
+    handlers={function(server_name) require('lspconfig')[server_name].setup({}) end}
+})
+
+
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+vim.diagnostic.config({
+  virtual_text = false,
+  severity_sort = true,
+  float = {
+    style = 'minimal',
+    border = 'rounded',
+    source = 'always',
+    header = '',
+    prefix = '',
+  },
+})
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local cmp_format = lsp_zero.cmp_format()
+
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+cmp.setup({
+    formatting=cmp_format,
+    preselect='item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+    window = {
+        documentation = cmp.config.window.bordered(),
+    },
+    sources = {
+        {name = 'path'},
+        {name = 'nvim_lsp'},
+        {name = 'nvim_lua'},
+        {name = 'buffer', keyword_length = 3},
+        {name = 'luasnip', keyword_length = 2},
+      },
+    mapping = cmp.mapping.preset.insert({
+    -- Only exists for lifetime of buffer 
+    -- i.e. any mappings used below will not override other mappings
+    ["<C-N>"] = cmp.mapping.select_prev_item(cmp_select), 
+    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select), 
+    ["<C-y>"] = cmp.mapping.confirm({select=true}), 
+    ["<C-Space>"] = cmp.mapping.complete(),}),
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end
+    }
+})
 --
 --lsp.set_preferences({
 --    sign_icons = {}
@@ -30,4 +81,4 @@ require('mason-lspconfig').setup({
 --lsp.setup_nvim_cmp({ mapping = cmp_mappings })
 --
 --
---lsp.setup()
+lsp_zero.setup()
--- lua/fak/packer.lua
+++ lua/fak/packer.lua
@@ -19,37 +19,20 @@ return require('packer').startup(function(use)
     use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
     -- themes 
-    use { "catppuccin/nvim", as = "catppuccin" }
+    use({ "catppuccin/nvim", as = "catppuccin" })
     use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-    use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    }
-    use({'hrsh7th/nvim-cmp'})
-    use({'hrsh7th/cmp-nvim-lsp'})
---    use {
---      'VonHeikemen/lsp-zero.nvim',
---      branch = 'v4.x',
---      requires = {
---        -- LSP Support
---        {'neovim/nvim-lspconfig'},             -- Required
---        {'williamboman/mason.nvim'},           -- Optional
---        {'williamboman/mason-lspconfig.nvim'}, -- Optional
---
---        -- Autocompletion
---        {'hrsh7th/nvim-cmp'},         -- Required
---        {'hrsh7th/cmp-nvim-lsp'},     -- Required
---        {'hrsh7th/cmp-buffer'},       -- Optional
---        {'hrsh7th/cmp-path'},         -- Optional
---        {'saadparwaiz1/cmp_luasnip'}, -- Optional
---        {'hrsh7th/cmp-nvim-lua'},     -- Optional
---
---        -- Snippets
---        {'L3MON4D3/LuaSnip'},             -- Required
---        {'rafamadriz/friendly-snippets'}, -- Optional
---      }
---    }    
+    use ({
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        'hrsh7th/nvim-cmp', 
+        'hrsh7th/cmp-nvim-lsp', 
+        'hrsh7th/cmp-path',
+        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-nvim-lua',
+        'L3MON4D3/LuaSnip',
+        'rafamadriz/friendly-snippets',
+    })
 end)
  
 
