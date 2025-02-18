ab2a22b249c06af37d6cb91d0a0ce3860915bb12
farooq-azam-khan <khanazamfarooq99@gmail.com>
2025-02-18 10:58:18 -0500

    added tmux package for nav

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,24 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
+    {
+      "christoomey/vim-tmux-navigator",
+      cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+        "TmuxNavigatorProcessList",
+      },
+      keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+      },
+    },
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -49,6 +49,7 @@ return {
         use_libuv_file_watcher = true,
       },
       window = {
+        position = "right",
         mappings = {
           ["l"] = "open",
           ["h"] = "close_node",
