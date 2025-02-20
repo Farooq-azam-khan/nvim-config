2bc025a97c0492fb32a7ac769a7b6b5801e472fb
farooq-azam-khan <khanazamfarooq99@gmail.com>
2025-02-20 15:17:02 -0500

    hidden files

--- lua/plugins/plugin-configs.lua
+++ lua/plugins/plugin-configs.lua
@@ -1,27 +1,29 @@
 return {
-    {
-      "folke/snacks.nvim",
-      opts = {
-        picker = {
-          sources = {
-            explorer = {
-              layout = { layout = { position = "right" } },
-            },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true,
+        ignored = true,
+        sources = {
+          explorer = {
+            layout = { layout = { position = "right" } },
           },
         },
-        explorer = {}, -- Make sure to include this at the top level
       },
+      explorer = {}, -- Make sure to include this at the top level
     },
-    -- change trouble config
-    {
-      "folke/trouble.nvim",
-      -- opts will be merged with the parent spec
-      opts = { use_diagnostic_signs = true },
-    },
-    -- change some telescope options and a keymap to browse plugin files
-    {
-      "nvim-telescope/telescope.nvim",
-      keys = {
+  },
+  -- change trouble config
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+  -- change some telescope options and a keymap to browse plugin files
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
         -- add a keymap to browse plugin files
         -- stylua: ignore
         {
@@ -29,15 +31,15 @@ return {
           function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
           desc = "Find Plugin File",
         },
-      },
-      -- change some options
-      opts = {
-        defaults = {
-          layout_strategy = "horizontal",
-          layout_config = { prompt_position = "top" },
-          sorting_strategy = "ascending",
-          winblend = 0,
-        },
+    },
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
       },
     },
-  }
+  },
+}
