686da5f85d20afb97c7ac38748b2e6dc77b3b40c
farooq-azam-khan <khanazamfarooq99@gmail.com>
2025-02-20 14:59:00 -0500

    updated with right values

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.editor" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -31,7 +30,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "catppuccin"} },
+  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
---
+++ lua/plugins/plugin-configs.lua
@@ -0,0 +1,43 @@
+return {
+    {
+      "folke/snacks.nvim",
+      opts = {
+        picker = {
+          sources = {
+            explorer = {
+              layout = { layout = { position = "right" } },
+            },
+          },
+        },
+        explorer = {}, -- Make sure to include this at the top level
+      },
+    },
+    -- change trouble config
+    {
+      "folke/trouble.nvim",
+      -- opts will be merged with the parent spec
+      opts = { use_diagnostic_signs = true },
+    },
+    -- change some telescope options and a keymap to browse plugin files
+    {
+      "nvim-telescope/telescope.nvim",
+      keys = {
+        -- add a keymap to browse plugin files
+        -- stylua: ignore
+        {
+          "<leader>fp",
+          function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+          desc = "Find Plugin File",
+        },
+      },
+      -- change some options
+      opts = {
+        defaults = {
+          layout_strategy = "horizontal",
+          layout_config = { prompt_position = "top" },
+          sorting_strategy = "ascending",
+          winblend = 0,
+        },
+      },
+    },
+  }
