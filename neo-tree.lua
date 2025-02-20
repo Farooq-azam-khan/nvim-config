61ff09f07ee61d6dd62a8177f523d7686b83fc04
farooq-azam-khan <khanazamfarooq99@gmail.com>
2025-02-20 13:33:47 -0500

    messed up custom config

--- neo-tree.lua
+++ neo-tree.lua
@@ -1,13 +1,6 @@
 return {
-  { import = "lazyvim.plugins.extras.neo-tree.nvim" },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
     opts = {
       filesystem = {
         filtered_items = {
