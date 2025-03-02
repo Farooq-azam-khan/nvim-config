9ec23eebc9666a4a6ea274d9191782858da2164d
farooq-azam-khan <khanazamfarooq99@gmail.com>
2025-03-02 09:30:17 -0500

    zen mode

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,9 +1,15 @@
--- Lua
-{
-  "folke/zen-mode.nvim",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+return {
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    plugins = {
+      tmux = {
+        enabled = true,
+      },
+    },
+  },
 }
