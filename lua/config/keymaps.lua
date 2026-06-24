e0ea8c81f595a8b24ab77301285606e297312663
farooq-azam-khan <khanazamfarooq99@gmail.com>
2026-06-24 15:32:32 -0400

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("n", "<leader>tw", function()
+  local save = vim.fn.winsaveview()
+  vim.cmd([[%s/\s\+$//e]])
+  vim.fn.winrestview(save)
+end, { desc = "Trim trailing whitespace" })
